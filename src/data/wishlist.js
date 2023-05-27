cf68968b42c51bf62bb623d7cd9b61c53d612073
Luzin7 <lvictor.dutra@gmail.com>
2023-05-27 00:34:14 -0300

    fix(wishlist.js): ajusta chaves com nomes errados

--- src/data/wishlist.js
+++ src/data/wishlist.js
@@ -1,16 +1,14 @@
 const WISHLIST = {
   movies: [
     {
-      id: 1,
+      id: 0,
       name: 'Barbie',
-      media:
-        'https://s2.glbimg.com/FmNhe17El_Iab8n7qo203-cBDGw=/0x0:480x600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2023/x/C/jqAddPQOijfNlCNKNEFg/339277575-166477655924790-1364217602656200753-n.jpeg',
+      img: 'https://s2.glbimg.com/FmNhe17El_Iab8n7qo203-cBDGw=/0x0:480x600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2023/x/C/jqAddPQOijfNlCNKNEFg/339277575-166477655924790-1364217602656200753-n.jpeg',
     },
     {
-      id: 3,
+      id: 0,
       name: 'O Menu',
-      media:
-        'https://br.web.img2.acsta.net/pictures/22/08/17/22/25/2724945.jpg',
+      img: 'https://br.web.img2.acsta.net/pictures/22/08/17/22/25/2724945.jpg',
     },
   ],
 };
