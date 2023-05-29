e605e93c3371aaa82f3dac52c693d6d05c2c19d9
Luzin7 <lvictor.dutra@gmail.com>
2023-05-29 17:48:05 -0300

    feat(wishlist.js): retira objeto

--- src/data/wishlist.js
+++ src/data/wishlist.js
@@ -5,11 +5,6 @@ const WISHLIST = {
       name: 'Barbie',
       img: 'https://s2.glbimg.com/FmNhe17El_Iab8n7qo203-cBDGw=/0x0:480x600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2023/x/C/jqAddPQOijfNlCNKNEFg/339277575-166477655924790-1364217602656200753-n.jpeg',
     },
-    {
-      id: 0,
-      name: 'O Menu',
-      img: 'https://br.web.img2.acsta.net/pictures/22/08/17/22/25/2724945.jpg',
-    },
   ],
 };
 
