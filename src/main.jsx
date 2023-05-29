0906b3f1298b72942c3204910ccb8f646872a87b
Luzin7 <lvictor.dutra@gmail.com>
2023-05-29 19:06:03 -0300

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ContentsProvider } from './contexts/LoadContents';
+import Index from './routes/index';
 
 import './styles/styles.css';
 import './styles/responsive.css';
-import Index from './routes/Index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
