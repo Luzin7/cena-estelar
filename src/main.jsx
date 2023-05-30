b960726841df4186bdb037959cfa928af6a23230
Luzin7 <lvictor.dutra@gmail.com>
2023-05-30 18:46:01 -0300

    update(main.jsx): retira import unitilizado

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { ContentsProvider } from './contexts/LoadContents';
 import Index from './routes/index';
 
 import './styles/styles.css';
-import './styles/responsive.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
