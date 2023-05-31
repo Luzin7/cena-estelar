28b4883d423224a64714aeb7ac1967293234c2e3
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:05:22 -0300

    remove

--- src/utils/paths.js
+++
@@ -1,9 +0,0 @@
-export const ROOT = '/';
-export const HOME = '/home';
-export const SEARCH = '/search';
-export const MOVIES = '/movies';
-export const SERIES = '/series';
-export const ANIMES = '/animes';
-export const MOVIE = 'movie/:id';
-export const SERIE = '/serie/:id';
-export const ANIME = '/anime/:id';
