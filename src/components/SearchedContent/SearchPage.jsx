0ce5b39c5878d98b1cffead677a3de3ec7018bad
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:14:45 -0300

    feat(SearchPage.jsx): cria página de conteudo pesquisado

---
+++ src/components/SearchedContent/SearchPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchedContent from './SearchedContent';
+
+function SearchPage() {
+  return (
+    <>
+      <SearchedContent />
+    </>
+  );
+}
+
+export default SearchPage;
