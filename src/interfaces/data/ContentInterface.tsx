fc51517717d3a0ad8cd93de5f9be710f67376b18
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:19:43 -0300

    feat(ContentInterface.tsx): cria interface de dados

---
+++ src/interfaces/data/ContentInterface.tsx
@@ -0,0 +1,11 @@
+interface ContentProp {
+  id: number;
+  name: string;
+  description: string;
+  longDescription: string;
+  rating: string;
+  img: string;
+  media: string;
+  genres: string[];
+}
+export default ContentProp;
