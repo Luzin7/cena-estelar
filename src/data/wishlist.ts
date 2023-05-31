d84c9957b7bc162748c33656ff2400ae4c3da6f4
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:17:11 -0300

    refact(wishlist.ts): coverte código para typescript

---
+++ src/data/wishlist.ts
@@ -0,0 +1,13 @@
+import WishlistType from "../types/data/WishlistType";
+
+const WISHLIST: WishlistType = {
+  movies: [
+    {
+      id: 0,
+      name: 'Barbie',
+      img: 'https://s2.glbimg.com/FmNhe17El_Iab8n7qo203-cBDGw=/0x0:480x600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2023/x/C/jqAddPQOijfNlCNKNEFg/339277575-166477655924790-1364217602656200753-n.jpeg',
+    },
+  ],
+};
+
+export default WISHLIST;
