d590d48c6b60c5b14885bfbf64ae1f4a3ea02346
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:08:25 -0300

    feat(main.jsx): passa contexto de pesquisa do usuario

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ContentsProvider } from './contexts/LoadContents';
+// eslint-disable-next-line import/no-named-as-default
 import Index from './routes/index';
 
 import './styles/styles.css';
+import { SearchProvider } from './contexts/Search';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContentsProvider>
-      <Index />
+      <SearchProvider>
+        <Index />
+      </SearchProvider>
     </ContentsProvider>
   </React.StrictMode>,
 );
