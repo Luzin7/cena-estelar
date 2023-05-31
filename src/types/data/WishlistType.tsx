5811df93f0c303bcf74e5c7960124dae6e9f11b1
Luzin7 <lvictor.dutra@gmail.com>
2023-05-31 16:23:29 -0300

    feat(WishlistType.tsx): cria type para dados

---
+++ src/types/data/WishlistType.tsx
@@ -0,0 +1,8 @@
+import WishlistProp from "../../interfaces/data/WishlistInterface";
+
+type WishlistType = {
+  movies?: WishlistProp[],
+  series?: WishlistProp[],
+};
+
+export default WishlistType;
