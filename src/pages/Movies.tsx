323eead3e2eb7ecd4791c480b6a600bcd6299d1a
Luzin7 <lvictor.dutra@gmail.com>
2023-06-01 17:47:58 -0300

    (Movies.tsx): converte código para typescript

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,13 @@
+import MoviesPage from '../components/Movies/MoviesPage';
+import Header from '../components/Header/Header';
+
+function Movies() {
+  return (
+    <>
+      <Header />
+      <MoviesPage />
+    </>
+  );
+}
+
+export default Movies;
