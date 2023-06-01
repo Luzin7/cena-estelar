9d622865309c7552ca04c34b43d03562e6de993d
Luzin7 <lvictor.dutra@gmail.com>
2023-06-01 17:47:41 -0300

    (Movie.tsx): converte código para typescript

---
+++ src/pages/Movie.tsx
@@ -0,0 +1,12 @@
+import MoviePage from '../components/Movie/MoviePage';
+
+function Movie() {
+  return (
+    <>
+      {/* <Header /> */}
+      <MoviePage />
+    </>
+  );
+}
+
+export default Movie;
