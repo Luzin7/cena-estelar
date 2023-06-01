80d0c418c2628d903eaadc8efcf10f497e9c2b74
Luzin7 <lvictor.dutra@gmail.com>
2023-06-01 17:48:41 -0300

    (Search.tsx): converte código para typescript

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header';
+import SearchPage from '../components/SearchedContent/SearchPage';
+
+function Search() {
+  return (
+    <>
+      <Header />
+      <SearchPage />
+    </>
+  );
+}
+
+export default Search;
