227eb500f601125297c4bac5cef017be733b32b4
Luzin7 <lvictor.dutra@gmail.com>
2023-06-04 13:36:09 -0300

    feat(Movies.tsx): adiciona rodapé

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,11 +1,13 @@
 import MoviesPage from '../components/Movies/MoviesPage';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 function Movies() {
   return (
     <>
       <Header />
       <MoviesPage />
+      <Footer />
     </>
   );
 }
