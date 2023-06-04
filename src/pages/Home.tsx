ac468c3242969ab9a99398aa65f62e03559cbf1f
Luzin7 <lvictor.dutra@gmail.com>
2023-06-04 13:35:46 -0300

    feat(Home.tsx): adiciona rodapé

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomePage from '../components/Home/HomePage';
 import Header from '../components/Header/Header';
+import Footer from './../components/Footer/Footer';
 
 function Home() {
   return (
     <>
       <Header />
       <HomePage />
+      <Footer />
     </>
   );
 }
