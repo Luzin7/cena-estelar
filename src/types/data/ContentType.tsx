64f989bd8cce4f56b8f125e5c102c960902005da
Luzin7 <lvictor.dutra@gmail.com>
2023-06-20 19:47:06 -0300

    refact(ContentType.tsx): torna conteudo assistido mais global

--- src/types/data/ContentType.tsx
+++ src/types/data/ContentType.tsx
@@ -1,8 +1,8 @@
-import ContentProp from "../../interfaces/data/ContentInterface";
+import ContentProp from '../../interfaces/data/ContentInterface';
 
-type ContentType = {
-  movies: ContentProp[];
-  series: ContentProp[];
-}
+type WatchedContentType = {
+  movies?: ContentProp[];
+  series?: ContentProp[];
+};
 
-export default ContentType;
+export default WatchedContentType;
