a1038e1e4ca47c1f63af5797f227bf8c80745e9f
Luzin7 <lvictor.dutra@gmail.com>
2023-06-20 21:53:31 -0300

    refact(MoviesByGenre.tsx): adiciona verificadores aos conteudos

--- src/components/Movies/MoviesByGenre.tsx
+++ src/components/Movies/MoviesByGenre.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ContentsContext } from '../../contexts/LoadContents';
 import Carousel from '../Carousel/Carousel';
@@ -16,23 +16,23 @@ function Content() {
         <>
           <Carousel
             title="Filmes de comédia"
-            contents={filterMoviesByGenre(allMovies, 'comédia')}
+            contents={filterMoviesByGenre(allMovies?.movies, 'comédia')}
           />
           <Carousel
             title="Filmes de drama"
-            contents={filterMoviesByGenre(allMovies, 'drama')}
+            contents={filterMoviesByGenre(allMovies?.movies, 'drama')}
           />
           <Carousel
             title="Filmes de suspense"
-            contents={filterMoviesByGenre(allMovies, 'suspense')}
+            contents={filterMoviesByGenre(allMovies?.movies, 'suspense')}
           />
           <Carousel
             title="Filmes de terror"
-            contents={filterMoviesByGenre(allMovies, 'terror')}
+            contents={filterMoviesByGenre(allMovies?.movies, 'terror')}
           />
           <Carousel
             title="Filmes de animação"
-            contents={filterMoviesByGenre(allMovies, 'animação')}
+            contents={filterMoviesByGenre(allMovies?.movies, 'animação')}
           />
         </>
       )}
