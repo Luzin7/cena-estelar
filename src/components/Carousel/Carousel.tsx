6322b0dd6b91f76f2c50815e7b0a5e1d88578cba
Luzin7 <lvictor.dutra@gmail.com>
2023-06-28 20:12:06 -0300

    feat(Carousel.tsx): adiciona responsividade ao slider

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,21 +1,54 @@
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { FreeMode } from 'swiper';
+import { getViewWidth } from '../../hooks/getViewWidth';
 import SectionProp from '../../interfaces/SearchedContent/FilteredSectionInterface';
-
 import 'swiper/css';
 import 'swiper/css/free-mode';
-
-import { FreeMode } from 'swiper';
+import { useEffect, useState } from 'react';
 
 function Carousel({ title, contents }: SectionProp) {
-  const windowWidth = window.innerWidth;
+  const [windowWidth, setWindowWidth] = useState(getViewWidth());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const responsiveSlideView = (windowWidth: number) => {
+    const mobileM = 375;
+    const tablet = 768;
+    const laptop = 1024;
+    const laptopL = 1440;
+    const desktop = 1920;
+
+    if (windowWidth < mobileM) {
+      return 3;
+    } else if (windowWidth >= mobileM && windowWidth < tablet) {
+      return 3;
+    } else if (windowWidth >= tablet && windowWidth < laptop) {
+      return 2;
+    } else if (windowWidth >= laptop && windowWidth < laptopL) {
+      return 4;
+    } else if (windowWidth >= laptopL && windowWidth < desktop) {
+      return 4;
+    } else {
+      return 6;
+    }
+  };
 
   return (
     <section className="categoriesSection">
       <h2 className="section_title">{title}</h2>
       <Swiper
-        slidesPerView={windowWidth <= 600 ? 3 : 6}
+        slidesPerView={responsiveSlideView(windowWidth)}
         grabCursor={true}
         freeMode={true}
         modules={[FreeMode]}
