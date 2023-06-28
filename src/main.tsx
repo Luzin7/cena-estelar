27824e9d53188d765c06cfa31523f303c34408be
Luzin7 <lvictor.dutra@gmail.com>
2023-06-28 20:11:22 -0300

    update(main.tsx): ajusta imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ContentsProvider } from './contexts/LoadContents';
+import { ContentsProvider } from './hooks/contexts/LoadContents';
+import { SearchProvider } from './hooks/contexts/Search';
 import Index from './routes/index';
-
 import './styles/styles.css';
-import { SearchProvider } from './contexts/Search';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
