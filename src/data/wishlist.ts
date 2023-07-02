a6d5dc5b2c3aec2afe28047efb689c1e924a07f4
Luzin7 <lvictor.dutra@gmail.com>
2023-07-02 13:09:11 -0300

    update(wishlist.ts): adiciona novos dados

--- src/data/wishlist.ts
+++ src/data/wishlist.ts
@@ -1,4 +1,4 @@
-import WishlistType from "../types/data/WishlistType";
+import WishlistType from '../types/data/WishlistType';
 
 const WISHLIST: WishlistType = {
   movies: [
@@ -6,6 +6,26 @@ const WISHLIST: WishlistType = {
       id: 0,
       name: 'Barbie',
       img: 'https://s2.glbimg.com/FmNhe17El_Iab8n7qo203-cBDGw=/0x0:480x600/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_5dfbcf92c1a84b20a5da5024d398ff2f/internal_photos/bs/2023/x/C/jqAddPQOijfNlCNKNEFg/339277575-166477655924790-1364217602656200753-n.jpeg',
+    },
+    {
+      id: 0,
+      name: 'Tetris',
+      img: 'https://br.web.img3.acsta.net/c_310_420/pictures/23/03/03/09/17/4907700.jpg',
+    },
+    {
+      id: 0,
+      name: 'The Flash',
+      img: 'https://p2.trrsf.com/image/fget/cf/1200/1600/middle/images.terra.com/2023/02/13/fo3remix0aat4f4-rl2ulqvkkwtx.jpg',
+    },
+    {
+      id: 0,
+      name: 'Ghost in the Shell',
+      img: 'https://cinepop.com.br/wp-content/uploads/2020/08/GITS-cinepop1.jpg',
+    },
+    {
+      id: 0,
+      name: 'La La Land',
+      img: 'https://m.media-amazon.com/images/I/91jrKX9xjQL.jpg',
     },
   ],
 };
