cc19e68f446ee6a77f71408ff5f718b592ff5803
Luzin7 <lvictor.dutra@gmail.com>
2023-07-02 13:12:25 -0300

    refact(Search.tsx): retira estados e props

--- src/hooks/contexts/Search.tsx
+++ src/hooks/contexts/Search.tsx
@@ -3,20 +3,15 @@ import ProviderProp from '../../interfaces/contexts/ContextsInterface';
 import SearchContextProps from '../../interfaces/contexts/SearchContextInterface';
 
 export const SearchContext = createContext<SearchContextProps>({
-  searchIsActive: false,
-  setSearchIsActive: () => {},
   userQuery: '',
   setUserQuery: () => {},
 });
 
 export const SearchProvider = ({ children }: ProviderProp) => {
-  const [searchIsActive, setSearchIsActive] = useState(false);
   const [userQuery, setUserQuery] = useState('');
 
   return (
-    <SearchContext.Provider
-      value={{ searchIsActive, setSearchIsActive, userQuery, setUserQuery }}
-    >
+    <SearchContext.Provider value={{ userQuery, setUserQuery }}>
       {children}
     </SearchContext.Provider>
   );
