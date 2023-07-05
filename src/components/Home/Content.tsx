ae43b3bfc592f5c85c6af57b07b2d7384d263299
Luzin7 <lvictor.dutra@gmail.com>
2023-07-05 20:10:08 -0300

    fix(Content.tsx): corrige erro que quebrava todo o site ao tentar acessar

--- src/components/Home/Content.tsx
+++ src/components/Home/Content.tsx
@@ -4,6 +4,8 @@ import { ContentsContext } from '../../hooks/contexts/LoadContents';
 import { bestRatings, lastSeens } from '../../functions/sortArrays';
 import FadeSwiper from '../Swiper';
 import filterMoviesByGenre from '../../functions/filterByGenre';
+import { BsTypeH1 } from 'react-icons/bs';
+import Loading from '../Loading';
 
 function Content() {
   const { allMovies, wishlist, allSeries } = useContext(ContentsContext);
@@ -19,22 +21,28 @@ function Content() {
 
   return (
     <main className="home mainContainer">
-      <FadeSwiper
-        contents={filterMoviesByGenre(allMovies?.movies, randomCategory())}
-      />
-      <Carousel
-        title="Filmes vistos recentemente"
-        contents={lastSeens(allMovies?.movies) || []}
-      />
-      <Carousel
-        title="Filmes bem avaliados"
-        contents={bestRatings(allMovies?.movies) || []}
-      />
-      <Carousel
-        title="Series vistas recentemente"
-        contents={lastSeens(allSeries?.series) || []}
-      />
-      <Carousel title="Em breve" contents={wishlist || []} />
+      {wishlist ? (
+        <>
+          <FadeSwiper
+            contents={filterMoviesByGenre(allMovies?.movies, randomCategory())}
+          />
+          <Carousel
+            title="Filmes vistos recentemente"
+            contents={lastSeens(allMovies?.movies) || []}
+          />
+          <Carousel
+            title="Filmes bem avaliados"
+            contents={bestRatings(allMovies?.movies) || []}
+          />
+          <Carousel
+            title="Series vistas recentemente"
+            contents={lastSeens(allSeries?.series) || []}
+          />
+          <Carousel title="Em breve" contents={wishlist || []} />
+        </>
+      ) : (
+        <Loading />
+      )}
     </main>
   );
 }
