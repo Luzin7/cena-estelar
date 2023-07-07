283d29a421a2f68f5b4d1443ae8edc457b8f2fb8
Luzin7 <lvictor.dutra@gmail.com>
2023-07-07 10:05:44 -0300

    cria rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,27 +7,40 @@ import {
   MOVIE,
   SEARCH,
   RECOMMENDATION,
+  LOGIN,
+  REGISTER,
 } from '../utils/paths';
 import Error from '../pages/Error';
-import Recommendation from '../pages/Recommendation';
+import Loading from '../components/Loading';
+import { PrivateRoutes } from '../helpers/protectRoutes';
 
 const Root = lazy(() => import('../pages/Root'));
+const Login = lazy(() => import('../pages/Login'));
+const Register = lazy(() => import('../pages/Register'));
 const Home = lazy(() => import('../pages/Home'));
 const Movies = lazy(() => import('../pages/Movies'));
 const Movie = lazy(() => import('../pages/Movie'));
 const SearchPage = lazy(() => import('../pages/Search'));
+const Recommendation = lazy(() => import('../pages/Recommendation'));
 
 function Index() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<h1>Carregando..</h1>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
+          {/* private routes */}
+          <Route element={<PrivateRoutes />}>
+            <Route path={RECOMMENDATION} element={<Recommendation />} />
+          </Route>
+          {/* public routes */}
           <Route path={ROOT} element={<Root />} />
+          <Route path={LOGIN} element={<Login />} />
+          <Route path={REGISTER} element={<Register />} />
           <Route path={HOME} element={<Home />} />
           <Route path={MOVIES} element={<Movies />}>
             <Route path={MOVIE} element={<Movie />} />
           </Route>
-          <Route path={RECOMMENDATION} element={<Recommendation />} />
+
           <Route path={SEARCH} element={<SearchPage />} />
           <Route path="*" element={<Error />} />
         </Routes>
