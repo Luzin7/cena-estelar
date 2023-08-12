8634d18bfec1d7d4877db6ebfc30855ecdddf691
Luzin7 <lvictor.dutra@gmail.com>
2023-08-12 18:35:37 -0300

    refact(Register.tsx): refatora e melhora estrutura

--- src/components/RegisterForm/Register.tsx
+++ src/components/RegisterForm/Register.tsx
@@ -1,36 +1,66 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   createUserWithEmailAndPassword,
   sendEmailVerification,
 } from 'firebase/auth';
 import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { z } from 'zod';
 import { UserDataContext } from '../../contexts/userData';
 import { auth } from '../../services/firebase';
+import { LOGIN } from '../../utils/paths';
 import AuthButtons from '../AuthButtons';
 import './css/register.css';
+
+const schemaRegisterForm = z.object({
+  userData: z.object({
+    email: z.string().min(4, 'Por favor, informe um email válido'),
+    password: z
+      .string()
+      .min(6, 'Por favor, insira uma senha válida')
+      .max(
+        16,
+        'Tamanho da senha maior do que 16 caracteres não são permitidos',
+      ),
+  }),
+});
+
+type formProps = z.infer<typeof schemaRegisterForm>;
+
 export function RegisterForm() {
-  const [formInfo, setFormInfo] = useState({
-    email: '',
-    password: '',
-  });
   const { setUserData } = useContext(UserDataContext);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { email, password } = formInfo;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<formProps>({
+    criteriaMode: 'all',
+    mode: 'all',
+    resolver: zodResolver(schemaRegisterForm),
+    defaultValues: {
+      userData: {
+        email: '',
+        password: '',
+      },
+    },
+  });
 
-  interface loginProp {
-    email: string;
-    password: string;
-  }
+  const handleFormSubmit = (data: formProps) => {
+    const { userData } = data;
 
-  function registerAuth({ email, password }: loginProp) {
     const emailRegex: RegExp = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     const passwordRegex =
       /^(?=.*[A-Z])(?=.*\d)(?!.*\s)(?!.*([a-zA-Z])\1{3,})(?!.*\.{17,}).{6,16}$/;
 
     setIsLoading(true);
 
-    if (!emailRegex.test(email) || !passwordRegex.test(password)) {
+    if (
+      !emailRegex.test(userData.email) ||
+      !passwordRegex.test(userData.password)
+    ) {
       alert(
         'Email ou senha não são válidos. Certifique de ter colocado um email válido ou uma senha que siga os padrões de seguranças: pelo menos uma letra maiúscula, um número e a senha deve ter, no mínimo, 6 caracteres.',
       );
@@ -38,7 +68,7 @@ export function RegisterForm() {
       return;
     }
 
-    createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(auth, userData.email, userData.password)
       .then((userCredential) => {
         const userData = userCredential.user;
         setUserData(userData);
@@ -50,54 +80,39 @@ export function RegisterForm() {
         console.log(error.message);
         setIsLoading(false);
       });
-  }
+  };
 
   return (
     <div className="container">
       <div className="form_wrapper">
-        <form
-          className="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
+        <form className="form" onSubmit={handleSubmit(handleFormSubmit)}>
           <h3>Crie sua conta</h3>
           <label htmlFor="email">E-mail</label>
           <input
-            value={email}
-            onChange={({ target }) =>
-              setFormInfo((prevFormInfo) => ({
-                ...prevFormInfo,
-                email: target.value,
-              }))
-            }
             type="email"
-            name="email"
             id="email"
-            required
+            {...register('userData.email')}
+            placeholder="Email"
           />
+          {errors.userData?.email?.message && (
+            <span>{errors.userData?.email?.message}</span>
+          )}
           <label htmlFor="password">Senha (mínimo de 6 caracteres)</label>
           <input
-            value={password}
-            onChange={({ target }) =>
-              setFormInfo((prevFormInfo) => ({
-                ...prevFormInfo,
-                password: target.value,
-              }))
-            }
             type="password"
-            name="password"
+            autoComplete="currentPassword"
             id="password"
-            required
+            {...register('userData.password')}
+            placeholder="Senha"
           />
-          <button
-            onClick={() => registerAuth({ email, password })}
-            disabled={isLoading ? true : false}
-          >
+          {errors.userData?.password?.message && (
+            <span>{errors.userData?.password?.message}</span>
+          )}
+          <button type="submit" disabled={isLoading ? true : false}>
             {isLoading ? 'Criando...' : 'Criar conta'}
           </button>
           <span>
-            Já tem uma conta? <Link to={'/login'}>Entre aqui.</Link>
+            Já tem uma conta? <Link to={LOGIN}>Entre aqui.</Link>
           </span>
           <AuthButtons />
         </form>
