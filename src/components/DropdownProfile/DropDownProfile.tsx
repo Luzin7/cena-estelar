b3208f7fed0688a759f14aebf48d6b72a4c031e3
Luzin7 <lvictor.dutra@gmail.com>
2023-08-12 18:34:15 -0300

    feat(DropDownProfile.tsx): implementa logout

--- src/components/DropdownProfile/DropDownProfile.tsx
+++ src/components/DropdownProfile/DropDownProfile.tsx
@@ -1,10 +1,28 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserDataContext } from '../../contexts/userData';
-import { RECOMMENDATION, ROOT } from '../../utils/paths';
+import { auth } from '../../services/firebase';
+import { HOME, RECOMMENDATION, ROOT } from '../../utils/paths';
 
 export default function DropDownProfile() {
-  const { userIsLogged } = useContext(UserDataContext);
+  const { userIsLogged, setUserIsLogged } = useContext(UserDataContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        console.log('Logout realizado com sucesso.');
+        setUserIsLogged(false);
+        navigate(HOME);
+
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Erro ao fazer logout:', error);
+      });
+  };
+
   return (
     <ul className="flex flex-col flex-wrap text-center min-w-[7vw] text-[0.6rem]">
       <li>
@@ -15,6 +33,11 @@ export default function DropDownProfile() {
       <li>
         <Link to={ROOT}>Contato e Suporte</Link>
       </li>
+      {userIsLogged && (
+        <li onClick={handleLogout}>
+          <a href="logout">Sair</a>
+        </li>
+      )}
     </ul>
   );
 }
