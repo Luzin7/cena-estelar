abfd91c76a2d4106eb695e7a55106968d773fa0b
Luan Victor <lvictor.dutra@gmail.com>
2023-08-13 18:53:23 -0300

    update(Content.tsx): altera titulos

--- src/components/Home/Content.tsx
+++ src/components/Home/Content.tsx
@@ -6,7 +6,7 @@ import { bestRatings, lastSeens } from '../../functions/sortArrays';
 import Loading from '../Loading';
 import FadeSwiper from '../Swiper';
 
-const Carousel = lazy(() => import('../Carousel/Carousel'));
+const Carousel = lazy(() => import('../Carousel/SwiperSlide'));
 
 function Content() {
   const { allMovies, wishlist, allSeries } = useContext(ContentsContext);
@@ -18,18 +18,12 @@ function Content() {
           <FadeSwiper
             contents={filterMoviesByGenre(allMovies, randomCategory()) || []}
           />
-          <Carousel
-            title="Filmes vistos recentemente"
-            movies={lastSeens(allMovies) || []}
-          />
+          <Carousel title="Filmes vistos" movies={lastSeens(allMovies) || []} />
           <Carousel
             title="Filmes bem avaliados"
             movies={bestRatings(allMovies) || []}
           />
-          <Carousel
-            title="Series vistas recentemente"
-            series={lastSeens(allSeries) || []}
-          />
+          <Carousel title="Series vistas" series={lastSeens(allSeries) || []} />
           <Carousel title="Em breve" wishList={wishlist} />
         </>
       ) : (
