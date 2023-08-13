f06b6b578ac1c40525c8c7fb7d76c3fb15aae6e9
Luan Victor <lvictor.dutra@gmail.com>
2023-08-13 19:02:06 -0300

    feat(Movie.tsx): adiciona header e footer

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,9 +1,13 @@
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header';
 import MoviePage from '../components/Movie/MoviePage';
 
 function Movie() {
   return (
     <>
+      <Header />
       <MoviePage />
+      <Footer />
     </>
   );
 }
