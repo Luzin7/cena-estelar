78ff792185d0bbbf1c72f628563da65d0c64fe4c
Luan Victor <lvictor.dutra@gmail.com>
2023-08-13 18:59:18 -0300

    refact(MoviesByGenre.tsx): ajusta rota

--- src/components/Movies/MoviesByGenre.tsx
+++ src/components/Movies/MoviesByGenre.tsx
@@ -1,50 +1,44 @@
 import { lazy, useContext } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
 import { ContentsContext } from '../../contexts/LoadContents';
 import filterMoviesByGenre from '../../functions/filterByGenre';
 
-const Carousel = lazy(() => import('../Carousel/Carousel'));
+const Carousel = lazy(() => import('../Carousel/SwiperSlide'));
 
 function Content() {
   const { allMovies } = useContext(ContentsContext);
-  const { pathname } = useLocation();
 
   return (
     <main className="home mainContainer">
-      {pathname.includes('/movies/movie/') ? (
-        <Outlet />
-      ) : (
-        <>
-          <Carousel
-            title="Comédia"
-            movies={filterMoviesByGenre(allMovies, 'comédia') || []}
-          />
-          <Carousel
-            title="Drama"
-            movies={filterMoviesByGenre(allMovies, 'drama') || []}
-          />
-          <Carousel
-            title="Ação"
-            movies={filterMoviesByGenre(allMovies, 'ação') || []}
-          />
-          <Carousel
-            title="Suspense"
-            movies={filterMoviesByGenre(allMovies, 'suspense') || []}
-          />
-          <Carousel
-            title="Terror"
-            movies={filterMoviesByGenre(allMovies, 'terror') || []}
-          />
-          <Carousel
-            title="Animação"
-            movies={filterMoviesByGenre(allMovies, 'animação') || []}
-          />
-          <Carousel
-            title="Aventura"
-            movies={filterMoviesByGenre(allMovies, 'aventura') || []}
-          />
-        </>
-      )}
+      <>
+        <Carousel
+          title="Comédia"
+          movies={filterMoviesByGenre(allMovies, 'comédia') || []}
+        />
+        <Carousel
+          title="Drama"
+          movies={filterMoviesByGenre(allMovies, 'drama') || []}
+        />
+        <Carousel
+          title="Ação"
+          movies={filterMoviesByGenre(allMovies, 'ação') || []}
+        />
+        <Carousel
+          title="Suspense"
+          movies={filterMoviesByGenre(allMovies, 'suspense') || []}
+        />
+        <Carousel
+          title="Terror"
+          movies={filterMoviesByGenre(allMovies, 'terror') || []}
+        />
+        <Carousel
+          title="Animação"
+          movies={filterMoviesByGenre(allMovies, 'animação') || []}
+        />
+        <Carousel
+          title="Aventura"
+          movies={filterMoviesByGenre(allMovies, 'aventura') || []}
+        />
+      </>
     </main>
   );
 }
